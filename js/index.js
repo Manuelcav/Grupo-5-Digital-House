6adb52e0a48482aa33f05b248a43352321c13a7e
AugustoRocca <arocca@udesa.edu.ar>
2021-11-16 12:01:39 -0300

    trbajando en la home. Solucioné el problema de api key pero sigo atrasado

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ let qs = location.search;
 let qsto = new URLSearchParams(qs);
 let id = qsto.get ('id');
 
-let url = `https://api.themoviedb.org/3/movie/popular/${id}`;
+let url = `https://api.themoviedb.org/3/movie/popular?api_key=6c457fc9dfa8d0a4c7572bd5162907c9/${id}`;
 
 fetch(url)
     .then(function(response){
@@ -14,6 +14,12 @@ fetch(url)
         let peliculaTitulo = document.querySelector('.Peliculas h3.PeliculasTexto');
         let peliculaImagen = document.querySelector('.Peliculas img');
         let peliculaEstreno = document.querySelector('.Peliculas h4.PeliculasTexto');
+
+       /* for (let i=0; i<5; i++) {
+            peliculaTitulo= 
+            peliculaImagen=
+            peliculaEstreno=
+        } */
 
         //actualizar datos
         peliculaTitulo.innerText = data.original_title;
