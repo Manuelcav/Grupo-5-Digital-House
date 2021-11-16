bb6a3eb61b098acd2180abf7d8814a39634de486
AugustoRocca <arocca@udesa.edu.ar>
2021-11-16 17:46:40 -0300

    tengo que cambiar el nombre de la clase

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-let url = 'https://api.themoviedb.org/3/movie/popular?api_key=6c457fc9dfa8d0a4c7572bd5162907c9';
+let urlPopulares = 'https://api.themoviedb.org/3/movie/popular?api_key=6c457fc9dfa8d0a4c7572bd5162907c9';
 
-fetch(url)
+fetch(urlPopulares)
     .then(function(response){
         return response.json();
     })
@@ -14,7 +14,7 @@ fetch(url)
             console.log(data.results[i]);
            peliculas += `<li>
                             <a href='detallepelicula.html?id=${data.results[i].id}' target="blanck">
-                                <img src='https://image.tmdb.org/t/p/w342/${data.results[i].poster_path}' alt="A quiet place 2" >
+                                <img src='https://image.tmdb.org/t/p/w342/${data.results[i].poster_path}' alt="${data.results[i].original_title}" >
                                 <h3 class="PeliculasTexto"> ${data.results[i].original_title}</h3>
                                 <h4 class="PeliculasTexto">${data.results[i].release_date}</h4>
                             </a>
@@ -26,3 +26,33 @@ fetch(url)
     .catch(function(error){
         console.log(error);
     })
+/*
+let urlMasValoradas = 'https://api.themoviedb.org/3/movie/top_rated?api_key=6c457fc9dfa8d0a4c7572bd5162907c9'
+
+fetch(urlMasValoradas)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+
+        let peliculasPopulares = document.querySelector('ul.Peliculas');
+        let peliculas = '';
+        
+        for (let i=0; i<5; i++) {
+            console.log(data.results[i]);
+           peliculas += `<li>
+                            <a href='detallepelicula.html?id=${data.results[i].id}' target="blanck">
+                                <img src='https://image.tmdb.org/t/p/w342/${data.results[i].poster_path}' alt="${data.results[i].original_title}" >
+                                <h3 class="PeliculasTexto"> ${data.results[i].original_title}</h3>
+                                <h4 class="PeliculasTexto">${data.results[i].release_date}</h4>
+                            </a>
+                        </li>`;
+        }
+
+        peliculasPopulares.innerHTML = peliculas
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+    */ 
