5e4c6e09b878327854401382ecd358b5e83c16dd
AugustoRocca <arocca@udesa.edu.ar>
2021-11-16 17:23:27 -0300

    progreso en la home

--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-/*let qs = location.search;
-let qsto = new URLSearchParams(qs);
-let id = qsto.get ('id');*/
-
 let url = 'https://api.themoviedb.org/3/movie/popular?api_key=6c457fc9dfa8d0a4c7572bd5162907c9';
 
 fetch(url)
@@ -10,30 +6,22 @@ fetch(url)
     })
     .then(function(data){
         console.log(data);
-        console.log(data.results[0].original_title);
-        console.log(data.results[2].poster_path);
-        console.log(data.release_date);
-        console.log(data.results);
 
-        let peliculaTitulo = document.querySelector('.Peliculas h3.PeliculasTexto');
-        let peliculaImagen = document.querySelector('.Peliculas img');
-        let peliculaEstreno = document.querySelector('.Peliculas h4.PeliculasTexto');
-
+        let peliculasPopulares = document.querySelector('ul.Peliculas');
+        let peliculas = '';
         
         for (let i=0; i<5; i++) {
             console.log(data.results[i]);
+           peliculas += `<li>
+                            <a href='detallepelicula.html?id=${data.results[i].id}' target="blanck">
+                                <img src='https://image.tmdb.org/t/p/w342/${data.results[i].poster_path}' alt="A quiet place 2" >
+                                <h3 class="PeliculasTexto"> ${data.results[i].original_title}</h3>
+                                <h4 class="PeliculasTexto">${data.results[i].release_date}</h4>
+                            </a>
+                        </li>`;
         }
-            /*
-            peliculaTitulo= `${data.results[i].original_title}`
-            peliculaImagen=
-            peliculaEstreno=
-         */
 
-        //actualizar datos
-        peliculaTitulo.innerText = data.original_title;
-        peliculaImagen.src = data.poster_path;
-        peliculaEstreno.innerText = data.release_date;
-
+        peliculasPopulares.innerHTML = peliculas
     })
     .catch(function(error){
         console.log(error);
