ed76a076c6f45af070502ebb6545fefa4f6e3cbf
AugustoRocca <arocca@udesa.edu.ar>
2021-11-16 16:05:23 -0300

    encontre el identificador results

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-let qs = location.search;
+/*let qs = location.search;
 let qsto = new URLSearchParams(qs);
-let id = qsto.get ('id');
+let id = qsto.get ('id');*/
 
-let url = `https://api.themoviedb.org/3/movie/popular?api_key=6c457fc9dfa8d0a4c7572bd5162907c9/${id}`;
+let url = 'https://api.themoviedb.org/3/movie/popular?api_key=6c457fc9dfa8d0a4c7572bd5162907c9';
 
 fetch(url)
     .then(function(response){
@@ -10,16 +10,24 @@ fetch(url)
     })
     .then(function(data){
         console.log(data);
+        console.log(data.results[0].original_title);
+        console.log(data.results[2].poster_path);
+        console.log(data.release_date);
+        console.log(data.results);
 
         let peliculaTitulo = document.querySelector('.Peliculas h3.PeliculasTexto');
         let peliculaImagen = document.querySelector('.Peliculas img');
         let peliculaEstreno = document.querySelector('.Peliculas h4.PeliculasTexto');
 
-       /* for (let i=0; i<5; i++) {
-            peliculaTitulo= 
+        
+        for (let i=0; i<5; i++) {
+            console.log(data.results[i]);
+        }
+            /*
+            peliculaTitulo= `${data.results[i].original_title}`
             peliculaImagen=
             peliculaEstreno=
-        } */
+         */
 
         //actualizar datos
         peliculaTitulo.innerText = data.original_title;
