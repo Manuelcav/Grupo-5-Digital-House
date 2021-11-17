49546c7ceeb7ffe760cb76fe97ced828da2f35a4
Clara Zemborain <zemborainm@udesa.edu.ar>
2021-11-17 12:39:25 -0300

    agregue fetch

--- js/detallepelicula.js
+++ js/detallepelicula.js
@@ -4,4 +4,13 @@ let id = qsto.get('id');
 
 let url = 'https://api.themoviedb.org/3/movie/{movie_id}';
 
-
+fetch(url)
+    .then(function(response){
+        return response.json;
+    })
+    .then(function(data){
+        console.log(data);
+    })
+    .catch(function(error){
+        console.log(error);
+    })
