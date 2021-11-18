f524bfcfabcb331962846ffd720b90ed324f52d6
AugustoRocca <arocca@udesa.edu.ar>
2021-11-18 18:00:21 -0300

    trabajando en generos

---
+++ js/genres.js
@@ -0,0 +1,24 @@
+let urlGenerosPeliculas = 'https://api.themoviedb.org/3/genre/movie/list?api_key=6c457fc9dfa8d0a4c7572bd5162907c9';
+
+fetch(urlGenerosPeliculas)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+
+        let listaGenerosPeliculas = document.querySelector('ul.listaGenerosPeliculas');
+        let generosPeliculas = '';
+        
+        for (let i=0; data.genres.length; i++) {
+            console.log(data.genres[i]);
+            generosPeliculas += `<li>
+                                    <a href="detail-genres.html?id=${data.genres[i].id}"><h3>${data.genres[i].name}</h3></a>
+                                </li>`;
+        }
+
+        listaGenerosPeliculas.innerHTML = generosPeliculas
+    })
+    .catch(function(error){
+        console.log(error);
+    })
