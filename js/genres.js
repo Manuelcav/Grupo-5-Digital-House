79117968de65c90aef9fb5215d33728d6de96c75
AugustoRocca <arocca@udesa.edu.ar>
2021-11-18 18:59:48 -0300

    termine pagina de generos

--- js/genres.js
+++ js/genres.js
@@ -10,7 +10,7 @@ fetch(urlGenerosPeliculas)
         let listaGenerosPeliculas = document.querySelector('ul.listaGenerosPeliculas');
         let generosPeliculas = '';
         
-        for (let i=0; data.genres.length; i++) {
+        for (let i=0; i<19; i++) {
             console.log(data.genres[i]);
             generosPeliculas += `<li>
                                     <a href="detail-genres.html?id=${data.genres[i].id}"><h3>${data.genres[i].name}</h3></a>
@@ -18,6 +18,31 @@ fetch(urlGenerosPeliculas)
         }
 
         listaGenerosPeliculas.innerHTML = generosPeliculas
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+
+let urlGenerosSeries = 'https://api.themoviedb.org/3/genre/tv/list?api_key=6c457fc9dfa8d0a4c7572bd5162907c9';
+
+fetch(urlGenerosSeries)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    
+        let listaGenerosSeries = document.querySelector('ul.listaGenerosSeries');
+        let generosSeries = '';
+            
+        for (let i=0; i<16; i++) {
+            console.log(data.genres[i]);
+            generosSeries += `<li>
+                                    <a href="detail-genres.html?id=${data.genres[i].id}"><h3>${data.genres[i].name}</h3></a>
+                                </li>`;
+        }
+    
+        listaGenerosSeries.innerHTML = generosSeries
     })
     .catch(function(error){
         console.log(error);
